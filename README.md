adaeec2542d5c56afd58525d6f9a3c9bee261daa
tanprabhu <tanurp28@gmail.com>
2024-06-20 01:09:38 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# meow-counter
+ It's a counter modified to play an audio, display transition of an image across the screen and change the background colour of the screen on clicking the increment button.
---
+++ script.js
@@ -0,0 +1,42 @@
+
+//DOM (document object model)- entire HTML document
+//'DOMContentLoaded' event- specific event in the lifecycle of a web page;useful for running scripts as soon as the document is ready, without waiting for all the external resources to load.
+
+document.addEventListener('DOMContentLoaded', function() {
+  const counterElement = document.getElementById('counter');
+  const incrementButton = document.getElementById('increment');
+  const decrementButton = document.getElementById('decrement');
+  const audio = document.getElementById('meow-inc');
+  const popOutGif = document.getElementById('meowing-cat');
+  const body=document.body;
+  
+  let counter = 0; //counter variable stores the values
+  
+  incrementButton.addEventListener('click', function() {
+      counter++;
+      counterElement.textContent = counter;
+      //play audio when increment button is clicked
+      
+        audio.play();
+        body.backgroundColor="lightblue";
+      
+      // Show the pop-out GIF
+      popOutGif.classList.add('show');
+
+      //change bg color
+      body.classList.add('change-background');
+
+      // Hide the GIF & changes bg colour after a delay of 5 seconds
+      //show and change background are classes
+      setTimeout(() => {
+          popOutGif.classList.remove('show');
+          body.classList.remove('change-background');
+          
+      }, 2500);
+  });
+  
+  decrementButton.addEventListener('click', function() {
+      counter--;
+      counterElement.textContent = counter;
+  });
+});
